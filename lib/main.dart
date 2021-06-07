22b29b76b1d9940256559bb39f39a31c42a9f7d5
Hasan <hasanuzzaman.dev@gmail.com>
2021-06-07 18:05:42 +0600

    crossAxisAlignment: CrossAxisAlignment.stretch its like match_parent

--- lib/main.dart
+++ lib/main.dart
@@ -12,18 +12,38 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.white10,
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            // margin: EdgeInsets.all(20.00),
-            // margin: EdgeInsets.symmetric(vertical: 20, horizontal: 50.0),
-            // margin: EdgeInsets.fromLTRB(10, 20, 30, 40),
-            margin: EdgeInsets.only(left: 50),
-            padding: EdgeInsets.all(20),
-            color: Colors.white,
-            child: Center(
-              child: Text('Hello'),
-            ),
+          child: Column(
+            //mainAxisSize: MainAxisSize.min,
+            // verticalDirection: VerticalDirection.up,
+            //verticalDirection: VerticalDirection.down,
+            //mainAxisAlignment: MainAxisAlignment.end,
+            // mainAxisAlignment: MainAxisAlignment.center,
+            //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            // mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                height: 100.0,
+                //width: 100.0,
+                color: Colors.white,
+                child: Center(
+                  child: Text('Container 1'),
+                ),
+              ),
+              Container(
+                height: 100,
+                //width: 100,
+                color: Colors.blueGrey,
+                child: Text('Container 2'),
+              ),
+              Container(
+                height: 100,
+                //width: 100,
+                color: Colors.blue,
+                child: Text('Container 3'),
+              )
+            ],
           ),
         ),
       ),
