396b33eeb857f564236696fdf7efd3eb35d4e467
Hasan <hasanuzzaman.dev@gmail.com>
2021-06-08 18:37:37 +0600

    font, icons and images

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,51 @@ class MyApp extends StatelessWidget {
                   letterSpacing: 2.5,
                   fontWeight: FontWeight.bold,
                 ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin:
+                    EdgeInsets.symmetric(vertical: 16.00, horizontal: 16.00),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.black54,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      '+8801921681616',
+                      style: TextStyle(
+                        color: Colors.black87,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.black87,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'hasan@gmail.com',
+                      style: TextStyle(
+                        color: Colors.black87,
+                      ),
+                    ),
+                  ],
+                ),
               )
             ],
           ),
